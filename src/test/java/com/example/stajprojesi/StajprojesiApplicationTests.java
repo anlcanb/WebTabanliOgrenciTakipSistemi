828783dbfab1ccdf631e92ee9419d28ecfcb6e4a
anlcanb <bostancianil84@gmail.com>
2025-07-09 14:11:59 +0300

    ilk commmit

---
+++ src/test/java/com/example/stajprojesi/StajprojesiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stajprojesi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StajprojesiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
